769ecf32da188fbff07c71ca676cc17ddba5e8a4
Fabi-Andrian <andrianirinafabirce2024@gmail.com>
2026-08-29 15:59:01 +0300

    Update database connection password in db.php

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@ function OuvrirBase()
 {
     $host = "gateway01.ap-southeast-1.prod.aws.tidbcloud.com";      // Exemple: gateway01.ap-southeast-1.prod.aws.tidbcloud.com
     $user = "bySeK84j6r5z3iE.root";      // Exemple: XXXXX.root
-    $pass = "";
+    $pass = "6bfmv35t0c8aFJeP";
     $dbname = "sys";               // Nom par défaut ou celui créé dans TiDB
     $port = 4000;                   // Port TiDB (par défaut 4000)
 
