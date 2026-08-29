4bcdb4431a9a3cdb02b1f21ee018a40dca6bdf9b
Fabi-Andrian <andrianirinafabirce2024@gmail.com>
2026-08-29 15:55:22 +0300

    Update database connection to use TiDB with SSL

--- config/db.php
+++ config/db.php
@@ -1,9 +1,16 @@
 <?php
 function OuvrirBase()
 {
-    $lien = mysqli_connect("localhost", "root", "", "colis_db");
-    if (!$lien)
-    {
+    $host = "VOTRE_HOST_TIDB";      // Exemple: gateway01.ap-southeast-1.prod.aws.tidbcloud.com
+    $user = "VOTRE_USER_TIDB";      // Exemple: XXXXX.root
+    $pass = "VOTRE_MOT_DE_PASSE";
+    $dbname = "test";               // Nom par défaut ou celui créé dans TiDB
+    $port = 4000;                   // Port TiDB (par défaut 4000)
+
+    $lien = mysqli_init();
+    mysqli_ssl_set($lien, NULL, NULL, NULL, NULL, NULL);
+    
+    if (!mysqli_real_connect($lien, $host, $user, $pass, $dbname, $port, NULL, MYSQLI_CLIENT_SSL)) {
         die("Impossible d'établir la connexion : " . mysqli_connect_error());
     }
     return $lien;
