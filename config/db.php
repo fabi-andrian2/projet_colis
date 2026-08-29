59791c8de504825a48be5a327e9f0d6c852b9e68
Fabi-Andrian <andrianirinafabirce2024@gmail.com>
2026-08-29 15:09:24 +0300

    Update database connection to use local server settings

--- config/db.php
+++ config/db.php
@@ -1,12 +1,7 @@
 <?php
 function OuvrirBase()
 {
-    $host = "sql310.infinityfree.com";
-    $user = "if0_42778909";
-    $pass = "PqC2Bo1HVp1fNRV";
-    $dbname = "if0_42778909_colis_db";
-
-    $lien = mysqli_connect($host, $user, $pass, $dbname);
+    $lien = mysqli_connect("localhost", "root", "", "colis_db");
     if (!$lien)
     {
         die("Impossible d'établir la connexion : " . mysqli_connect_error());
